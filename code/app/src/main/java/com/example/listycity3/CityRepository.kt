a8a8601199aecb3d7488de1729e9dbf819b7689b
N0tS4mmy <sbgillie@ualberta.ca>
2026-09-16 19:33:23 -0600

    lab3

--- code/app/src/main/java/com/example/listycity3/CityRepository.kt
+++ code/app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -1,7 +1,11 @@
 package com.example.listycity3
 
+import androidx.compose.runtime.mutableStateListOf
+
+
+
 class CityRepository {
-    private val _cities = listOf(
+    private val _cities = mutableStateListOf(
         City("Edmonton", "AB"),
         City("Vancouver", "BC"),
         City("Toronto", "ON")
@@ -9,4 +13,7 @@ class CityRepository {
 
     val cities: List<City>
         get() = _cities
+    fun addCity(city: City) {
+        _cities.add(city)
+    }
 }
